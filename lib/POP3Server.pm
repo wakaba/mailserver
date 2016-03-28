3405fc3e455ceb95fe87986f4d3138c8166f97ae
Wakaba <wakaba@suikawiki.org>
2016-03-28 20:10:44 +0900

    POP3 commands

--- lib/POP3Server.pm
+++ lib/POP3Server.pm
@@ -2,11 +2,15 @@ package POP3Server;
 use strict;
 use warnings;
 use AnyEvent::Handle;
+use Promise;
 
 sub D ($) { warn "DEBUG: $_[0]\n" }
 
 sub new ($) {
-  return bless {state => 'AUTHORIZATION'}, $_[0];
+  return bless {state => 'AUTHORIZATION',
+                processing => 0,
+                lines => [],
+                deleted => {}}, $_[0];
 } # new
 
 sub onauth ($;$) {
@@ -16,24 +20,49 @@ sub onauth ($;$) {
   return $_[0]->{onauth} || sub { 0 };
 } # onauth
 
+sub onstat ($;$) {
+  if (@_ > 1) {
+    $_[0]->{onstat} = $_[1];
+  }
+  return $_[0]->{onstat} || sub { {count => 0, size => 0} };
+} # onstat
+
+sub onlist ($;$) {
+  if (@_ > 1) {
+    $_[0]->{onlist} = $_[1];
+  }
+  return $_[0]->{onlist} || sub { [] };
+} # onlist
+
+sub onlist_of ($;$) {
+  if (@_ > 1) {
+    $_[0]->{onlist_of} = $_[1];
+  }
+  return $_[0]->{onlist_of} || sub { undef };
+} # onlist_of
+
+sub onretr ($;$) {
+  if (@_ > 1) {
+    $_[0]->{onretr} = $_[1];
+  }
+  return $_[0]->{onretr} || sub { undef };
+} # onretr
+
+sub ondelete ($;$) {
+  if (@_ > 1) {
+    $_[0]->{ondelete} = $_[1];
+  }
+  return $_[0]->{ondelete} || sub { 1 };
+} # ondelete
+
 sub set_fh ($$) {
   my $self = $_[0];
   $self->{handle} = AnyEvent::Handle->new
       (fh => $_[1],
        on_read => sub {
          if ($_[0]->{rbuf} =~ /\A(.*?)\x0D\x0A/s) {
-           my $line = $1;
-           if ($line =~ s/\A([A-Za-z]{3,4}) //) {
-             my $command = $1;
-             $command =~ tr/a-z/A-Z/;
-             $self->_command ($command, $line);
-           } elsif ($line =~ /\A([A-Za-z]{3,4})\z/) {
-             my $command = $1;
-             $command =~ tr/a-z/A-Z/;
-             $self->_command ($command, '');
-           } else {
-             return $self->fatal_error ("Bad command line");
-           }
+           push @{$self->{lines}}, $1;
+           $self->_lines;
            $_[0]->{rbuf} = '';
          } elsif (length $_[0]->{rbuf} > 512) {
            return $self->fatal_error ("Line too long");
@@ -46,10 +75,12 @@ sub set_fh ($$) {
        on_error => sub {
          D "error $_[2]";
          $self->{handle}->destroy;
+         delete $self->{handle};
        },
        on_eof => sub {
          D "eof";
          $self->{handle}->destroy;
+         delete $self->{handle};
        });
   $self->ok ('Hello, what is your USER name?');
 } # set_fh
@@ -64,6 +95,7 @@ sub fatal_error ($$) {
   D "Fatal error: $_[1]";
   $self->{handle}->push_write ("-ERR $_[1]\x0D\x0A");
   $self->{handle}->push_shutdown;
+  $self->{fatal} = 1;
 } # fatal_error
 
 sub error ($$) {
@@ -71,33 +103,183 @@ sub error ($$) {
   $self->{handle}->push_write ("-ERR $_[1]\x0D\x0A");
 } # error
 
+sub _lines ($) {
+  my $self = $_[0];
+  return if $self->{processing} or $self->{fatal};
+  return unless defined $self->{handle};
+  return unless @{$self->{lines}};
+  return Promise->resolve->then (sub {
+    $self->{processing} = 1;
+    my $line = shift @{$self->{lines}};
+    if ($line =~ s/\A([A-Za-z]{3,4}) //) {
+      my $command = $1;
+      $command =~ tr/a-z/A-Z/;
+      return $self->_command ($command, $line);
+    } elsif ($line =~ /\A([A-Za-z]{3,4})\z/) {
+      my $command = $1;
+      $command =~ tr/a-z/A-Z/;
+      return $self->_command ($command, '');
+    } else {
+      $self->{processing} = 0;
+      return $self->fatal_error ("Bad command line");
+    }
+  })->then (sub { $self->_lines });
+} # _lines
+
 sub _command ($$$) {
   my ($self, $command, $args) = @_;
   if ($self->{state} eq 'AUTHORIZATION') {
     if ($command eq 'USER') {
       $self->{user} = $args;
+      $self->{processing} = 0;
       return $self->ok ('What is your PASS?');
     } elsif ($command eq 'PASS') {
       if (not defined $self->{user}) {
+        $self->{processing} = 0;
         return $self->error ('No USER before PASS');
       }
       $self->{pass} = $args;
-      if ($self->onauth->($self)) {
-        $self->{state} = 'TRANSACTION';
-        return $self->ok ('Welcome');
-      } else {
-        return $self->error ('Bad USER or PASS');
-      }
+      return Promise->resolve->then (sub {
+        return $self->onauth->($self);
+      })->then (sub {
+        if ($_[0]) {
+          $self->{state} = 'TRANSACTION';
+          $self->{processing} = 0;
+          return $self->ok ('Welcome');
+        } else {
+          $self->{processing} = 0;
+          return $self->error ('Bad USER or PASS');
+        }
+      });
     } elsif ($command eq 'QUIT') {
       $self->ok ('Bye');
+      $self->{processing} = 0;
       return $self->{handle}->push_shutdown;
     }
   } elsif ($self->{state} eq 'TRANSACTION') {
-
-
+    if ($command eq 'STAT') {
+      if (length $args) {
+        $self->{processing} = 0;
+        return $self->error ('Bad arguments');
+      }
+      return Promise->resolve->then (sub {
+        return $self->onstat->($self);
+      })->then (sub {
+        my $result = $_[0];
+        my $count = $result->{count} || 0;
+        my $size = $result->{size} || 0;
+        $self->ok (sprintf "%d %d", $count, $size);
+        return $self->{processing} = 0;
+      });
+    } elsif ($command eq 'LIST') {
+      if ($args =~ /\A[0-9]+\z/) {
+        $args = 0+$args;
+        if ($self->{deleted}->{$args}) {
+          $self->error ("Message not found");
+          return $self->{processing} = 0;
+        } else {
+          return Promise->resolve->then (sub {
+            return $self->onlist_of->($self, $args);
+          })->then (sub {
+            my $result = $_[0];
+            if (defined $result) {
+              $self->ok (sprintf '%d %d', $args, $result->{size});
+            } else {
+              $self->error ("Message not found");
+            }
+            return $self->{processing} = 0;
+          });
+        }
+      } elsif ($args eq '') {
+        return Promise->resolve->then (sub {
+          return $self->onlist->($self);
+        })->then (sub {
+          my $result = $_[0];
+          $self->ok ('...');
+          for (@$result) {
+            next if $self->{deleted}->{0+$_->{number}};
+            $self->{handle}->push_write (sprintf "%d %d\x0D\x0A", $_->{number}, $_->{size});
+          }
+          $self->{handle}->push_write (".\x0D\x0A");
+          return $self->{processing} = 0;
+        });
+      } else {
+        $self->error ("Bad arguments");
+        return $self->{processing} = 0;
+      }
+    } elsif ($command eq 'RETR') {
+      unless ($args =~ /\A[0-9]+\z/) {
+        $self->error ("Bad arguments");
+        return $self->{processing} = 0;
+      }
+      $args = 0+$args;
+      if ($self->{deleted}->{$args}) {
+        $self->error ("Message not found");
+        return $self->{processing} = 0;
+      }
+      return Promise->resolve->then (sub {
+        return $self->onretr->($self, $args);
+      })->then (sub {
+        unless (defined $_[0]) {
+          $self->error ("Message not found");
+          return $self->{processing} = 0;
+        }
+        $self->ok ('...');
+        pos ($_[0]) = 0;
+        while ($_[0] =~ /\G(.*?\x0D\x0A)/gs) {
+          $self->{handle}->push_write ('.');
+          $self->{handle}->push_write ($1);
+        }
+        if ($_[0] =~ /\G(.+)\z/gs) {
+          $self->{handle}->push_write ('.');
+          $self->{handle}->push_write ($1);
+          $self->{handle}->push_write ("\x0D\x0A");
+        }
+        $self->{handle}->push_write (".\x0D\x0A");
+        return $self->{processing} = 0;
+      });
+    } elsif ($command eq 'DELE') {
+      unless ($args =~ /\A[0-9]+\z/) {
+        $self->error ("Bad arguments");
+        return $self->{processing} = 0;
+      }
+      $args = 0+$args;
+      if ($self->{deleted}->{$args}) {
+        $self->error ("Message not found");
+        return $self->{processing} = 0;
+      }
+      $self->{deleted}->{$args} = 1;
+      $self->ok ("Deleted");
+      return $self->{processing} = 0;
+    } elsif ($command eq 'RSET') {
+      unless ($args eq '') {
+        $self->error ("Bad arguments");
+        return $self->{processing} = 0;
+      }
+      $self->{deleted} = {};
+      $self->ok ('Rolled back');
+      return $self->{processing} = 0;
+    } elsif ($command eq 'NOOP') {
+      $self->ok ('Hi');
+      return $self->{processing} = 0;
+    } elsif ($command eq 'QUIT') {
+      return Promise->resolve->then (sub {
+        return $self->ondelete->($self);
+      })->then (sub {
+        if ($_[0]) {
+          $self->ok ('Bye');
+        } else {
+          $self->error ('Failed to save changes');
+        }
+        $self->{processing} = 0;
+        $self->{fatal} = 1;
+        $self->{handle}->push_shutdown;
+      });
+    }
   }
 
   $self->error ("Unknown command");
+  return $self->{processing} = 0;
 } # _command
 
 1;
--- pop3d.pl
+++ pop3d.pl
@@ -12,6 +12,22 @@ tcp_server $host, $port, sub {
   warn "Remote: $remote_host:$remote_port";
 
   my $server = POP3Server->new;
+  $server->onauth (sub {
+    my $self = $_[0];
+    if ($self->{user} eq 'user' and $self->{pass} eq 'pass') {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+  $server->onretr (sub {
+    my $id = $_[1];
+    if ($id == 1) {
+      return "abea\x0D\x0Axyqtea\x0D\x0A.aaa\x0D.\x0A...";
+    } else {
+      return undef;
+    }
+  });
   $server->set_fh ($fh);
 };
 
